e6396b75d7d4b9d55a255937cdb5ffc7c3822821
cuuj69 <mensahjefferson69@gmail.com>
2023-01-03 21:07:10 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press any key to continue....");
+            Console.ReadLine();
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine($"Greetings: {reply.Message}");
+            Console.WriteLine("press any key to exit..");
+            Console.ReadLine();
+
+          
+        }
+    }
+}
